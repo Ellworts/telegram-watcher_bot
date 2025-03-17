5c916b3c61858ff650194de809fb1eaeadf4e592
Misha Kuptsov <azazin1955@gmail.com>
2025-03-17 16:14:44 +0000

    env fix

--- src/bot.js
+++ src/bot.js
@@ -4,7 +4,19 @@ const axios = require("axios");
 const { BOT_TOKEN, FIREBASE_CREDENTIALS_PATH, OPENAI_API_KEY } = require("./config");
 const path = require("path");
 
-const serviceAccount = require('./telebot-watcher-firebase-adminsdk-fbsvc-780d9431be.json');
+const serviceAccount = {
+  type: "service_account",
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
