884ab690dd7d5d2e9506816d10a6786b16248c5f
Misha Kuptsov <azazin1955@gmail.com>
2025-03-17 16:19:31 +0000

    env fix

--- src/bot.js
+++ src/bot.js
@@ -6,16 +6,16 @@ const path = require("path");
 
 const serviceAccount = {
   type: "service_account",
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+  private_key_id: FIREBASE_PRIVATE_KEY_ID,
+  project_id: FIREBASE_PROJECT_ID,
+  private_key: FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: FIREBASE_CLIENT_EMAIL,
+  client_id: FIREBASE_CLIENT_ID,
+  auth_uri: FIREBASE_AUTH_URI,
+  token_uri: FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: FIREBASE_UNIVERSE_DOMAIN
 };
 
 admin.initializeApp({
